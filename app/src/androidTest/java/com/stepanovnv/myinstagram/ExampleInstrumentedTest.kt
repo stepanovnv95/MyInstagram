a7f4b9530939475ae2e31c2cce14db8427d611d7
Stepanov Nikita <stepanovnv95@gmail.com>
2019-11-28 19:24:06 +0400

    Finally fix package naming

--- app/src/androidTest/java/com/stepanovnv/myinstagram/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/stepanovnv/myinstagram/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("stepanovnv.com.myinstagram", appContext.packageName)
+        assertEquals("com.stepanovnv.myinstagram", appContext.packageName)
     }
 }
