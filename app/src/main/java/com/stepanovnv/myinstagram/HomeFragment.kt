8f1b9d4ec39e7f3b9514d3731f8cabd4778ef32e
Stepanov Nikita <stepanovnv95@gmail.com>
2019-11-29 22:29:23 +0400

    Created simple list fragment

---
+++ app/src/main/java/com/stepanovnv/myinstagram/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.stepanovnv.myinstagram
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.ListFragment
+
+
+class HomeFragment : ListFragment() {
+
+    private val catNames = listOf("Рыжик", "Барсик", "Мурзик",
+        "Мурка", "Васька", "Томасина", "Кристина", "Пушок", "Дымка",
+        "Кузя", "Китти", "Масяня", "Симба")
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val adapter = ArrayAdapter<String>(activity as Context, android.R.layout.simple_list_item_1, catNames)
+        listAdapter = adapter
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, null)
+    }
+
+}
