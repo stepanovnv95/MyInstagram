8caeeaf31e283f8889fd836db57ad02312eee4ea
Stepanov Nikita <stepanovnv95@gmail.com>
2019-11-30 16:45:13 +0400

    Cleanup MainActivity

--- app/src/main/java/com/stepanovnv/myinstagram/MainActivity.kt
+++ app/src/main/java/com/stepanovnv/myinstagram/MainActivity.kt
@@ -2,41 +2,12 @@ package com.stepanovnv.myinstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import android.util.Log
-//import android.view.View
-//import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-//    private val _tag = "MainActivity"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        val fragmentManager = supportFragmentManager
-//        val fragmentTransaction = fragmentManager.beginTransaction()
-//        val postFragment = PostFragment()
-//        fragmentTransaction.add(R.id.main_fragment_container, postFragment)
     }
-
-//    private fun setupBottomMenu() {
-//        bottom_nav_view.setOnNavigationItemSelectedListener {
-//            when (it.itemId) {
-//                R.id.action_home -> {
-//                    homeTextView.visibility = View.VISIBLE
-//                    favoritesTextView.visibility = View.GONE
-//                }
-//                R.id.action_favorites -> {
-//                    homeTextView.visibility = View.GONE
-//                    favoritesTextView.visibility = View.VISIBLE
-//                }
-//                else -> {
-//                    Log.e(_tag, "Unknown menu item: $it")
-//                }
-//            }
-//            true
-//        }
-//    }
 }
