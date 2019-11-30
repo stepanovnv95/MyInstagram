a1d9f6bd4672be4f495f8da182b8583ceeab4c08
Stepanov Nikita <stepanovnv95@gmail.com>
2019-11-30 18:08:13 +0400

    First working http request

--- app/src/main/java/com/stepanovnv/myinstagram/HomeFragment.kt
+++ app/src/main/java/com/stepanovnv/myinstagram/HomeFragment.kt
@@ -2,28 +2,43 @@ package com.stepanovnv.myinstagram
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import androidx.fragment.app.ListFragment
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 
 class HomeFragment : ListFragment() {
 
-    private val catNames = listOf("Рыжик", "Барсик", "Мурзик",
-        "Мурка", "Васька", "Томасина", "Кристина", "Пушок", "Дымка",
-        "Кузя", "Китти", "Масяня", "Симба")
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        val adapter = PostAdapter(activity as Context, android.R.layout.simple_list_item_1, catNames)
-        listAdapter = adapter
-    }
+    private var _swipeRefreshLayout: SwipeRefreshLayout? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, null)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        _swipeRefreshLayout = view.findViewById(R.id.swipe_refresh)
+        refresh()
+        return view
+    }
+
+    private fun refresh() {
+        _swipeRefreshLayout?.isRefreshing = true
+
+        val url = "http://v-wall.net/ibcon/auth.php?response_format=json"
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                Log.d("HomeFragment", "Response: %s".format(response.toString()))
+            },
+            Response.ErrorListener { error ->
+                Log.d("HomeFragment", "Error: %s".format(error.toString()))
+            }
+        )
+        val queue = Volley.newRequestQueue(activity as Context)
+        queue.add(jsonObjectRequest)
     }
 
 }
