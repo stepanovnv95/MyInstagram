817ee97fdd7cdd9ee480d67d4ab30068e02a584a
Stepanov Nikita <stepanovnv95@gmail.com>
2019-12-02 19:24:03 +0400

    Move activities to subdirectory

--- app/src/main/java/com/stepanovnv/myinstagram/activities/MainActivity.kt
+++ app/src/main/java/com/stepanovnv/myinstagram/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.stepanovnv.myinstagram
+package com.stepanovnv.myinstagram.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.stepanovnv.myinstagram.R
 
 
 class MainActivity : AppCompatActivity() {
