8f21521c0768f23e34da5fdbfdddcfdb39208f3d
Stepanov Nikita <stepanovnv95@gmail.com>
2019-12-14 12:50:15 +0400

    Realize about activity

--- app/src/main/java/com/stepanovnv/myinstagram/activities/ProfileActivity.kt
+++ app/src/main/java/com/stepanovnv/myinstagram/activities/ProfileActivity.kt
@@ -1,15 +1,14 @@
 package com.stepanovnv.myinstagram.activities
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.stepanovnv.myinstagram.R
 
 
 class ProfileActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
     }
 
